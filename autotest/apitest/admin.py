182daa5d1683c64e01e40a1a69a36ac531856b5a
meng <1137608614>
2020-07-03 22:26:43 +0800

    流程接口功能后台开发完成

--- autotest/apitest/admin.py
+++ autotest/apitest/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from apitest.models import Apitest, Apistep
+
+
+# TabularInline --横向的以表格的形式展示数据
+class ApistepAdmin(admin.TabularInline):
+    list_display = ['apiname', 'apiurl', 'apiparamvalue', 'apimethod', 'apiresult', 'apistatus', 'create_time', 'id',
+                    'apitest']
+    model = Apistep
+    extra = 1
+
+
+@admin.register(Apitest)
+class ApitestAdmin(admin.ModelAdmin):
+    list_display = ['apitestname', 'apitester', 'apitestresult', 'create_time', 'id']
+    inlines = [ApistepAdmin]
--- autotest/apitest/models.py
+++ autotest/apitest/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
 # Create your models here.
+
+from product.models import Product
+
+
+# 业务场景接口表
+class Apitest(models.Model):
+    # 关联产品ID，其中product是应用名，Product是表名
+    Product = models.ForeignKey('product.Product', on_delete=models.CASCADE, null=True)
+    # 流程接口名称
+    apitestname = models.CharField('流程接口名称', max_length=64)
+    # 流程接口描述
+    apitestdesc = models.CharField('描述', max_length=64, null=True)
+    # 测试负责人
+    apitester = models.CharField('测试负责人',max_length=16)
+    # 流程接口结果
+    apitestresult = models.BooleanField('测试结果')
+    # 创建时间,默认获取当前时间
+    create_time = models.DateTimeField('创建时间', auto_now=True)
+
+    # 数据库表名
+    class Meta:
+        verbose_name = '流程场景接口'
+        verbose_name_plural = '流程场景接口'
+
+    def __str__(self):
+        return self.apitestname
+
+
+# 业务场景步骤接口用例表
+class Apistep(models.Model):
+    # 关联接口用例ID
+    Apitest = models.ForeignKey(Apitest,on_delete=models.CASCADE)
+    # 接口标题
+    apiname = models.CharField('接口名称',max_length=100)
+    # 接口地址
+    apiurl = models.CharField('url地址',max_length=200)
+    # 请求参数和值
+    apiparamvalue = models.CharField('请求参数和值',max_length=800)
+    # 请求方法
+    REQUEST_METHOD = (('get','get'),('post','post'),('delete','delete'),('patch','patch'))
+    apimethod = models.CharField(verbose_name='请求方法',choices=REQUEST_METHOD,default='get',max_length=200,null=True)
+    # 预期结果
+    apiresult = models.CharField('预期结果',max_length=200)
+    # 测试结果
+    apistatus = models.BooleanField('是否通过')
+    # 创建时间
+    create_time = models.DateTimeField('创建时间',auto_now=True)
+
+    def __str__(self):
+        return self.apiname
